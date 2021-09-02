4ee5d7b73bfbd120c477b48960bea148204b5d20
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-09-02 11:02:30 +0100

    add accounthelp and accountTest

--- app/AccountantHelper.php
+++ app/AccountantHelper.php
@@ -4,7 +4,7 @@ namespace App;
 
 class AccountantHelper {
 
-    function findProfit($amount){
+    public function findProfit($amount){
         $profitPercent = 10;
 
         return $profitPercent * $amount / 100;
--- tests/Unit/AccountantTest.php
+++ tests/Unit/AccountantTest.php
@@ -9,8 +9,8 @@ class AccountantTest extends TestCase {
 
     /** @test */
     function it_can_find_profit(){
-        $profit = AccountantHelper::findProfit(100);
-
+        $AccountantHelper = New AccountantHelper;
+        $profit=$AccountantHelper->findProfit(100);
         $this->assertEquals(10, $profit);
 
         $this->assertLessThan(100, $profit);
